720836325b95c6a9b501692134d0aaaae521ce16
Arvin Garcia <arvin@arvingarcia.com>
2026-01-14 20:39:02 +0800

    feat: footer component

--- app/ui/footer.tsx
+++ app/ui/footer.tsx
@@ -1,3 +1,17 @@
 export default function Footer() {
-  return <p>This is a footer.</p>;
+	return (
+		<footer className="m-auto p-6 text-(--color-gray-1)">
+			<p>
+				Made by{" "}
+				<a
+					href="https://github.com/arvingarciabtw"
+					target="_blank"
+					rel="noopener noreferrer"
+					className="text-(--color-primary) underline hover:no-underline"
+				>
+					@arvingarciabtw
+				</a>
+			</p>
+		</footer>
+	);
 }
