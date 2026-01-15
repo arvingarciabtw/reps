d8fedf0d624856c273759213d88fb9f770e43c00
Arvin Garcia <arvin@arvingarcia.com>
2026-01-15 14:07:08 +0800

    feat: include custom fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,33 +1,51 @@
 import type { Metadata } from "next";
-import { Atkinson_Hyperlegible_Next } from "next/font/google";
+import {
+	Atkinson_Hyperlegible_Next,
+	Atkinson_Hyperlegible_Mono,
+	Zalando_Sans_Expanded,
+} from "next/font/google";
 import "./globals.css";
 
 const atkinsonHyperlegibleNext = Atkinson_Hyperlegible_Next({
-  variable: "--font-atkinson-hyperlegible-next",
-  subsets: ["latin"],
+	variable: "--font-atkinson-hyperlegible-next",
+	subsets: ["latin"],
+	fallback: ["Arial", "sans-serif"],
+});
+
+const atkinsonHyperlegibleMono = Atkinson_Hyperlegible_Mono({
+	variable: "--font-atkinson-hyperlegible-mono",
+	subsets: ["latin"],
+	fallback: ["Arial", "sans-serif"],
+});
+
+const zalandoSansExpanded = Zalando_Sans_Expanded({
+	variable: "--font-zalando-sans-expanded",
+	subsets: ["latin"],
+	fallback: ["Arial", "sans-serif"],
 });
 
 export const metadata: Metadata = {
-  title: "Reps",
-  description: "A spaced repetition app that supports code syntax highlighting",
-  metadataBase: new URL("https://reps.arvingarcia.com"),
-  applicationName: "Reps",
-  keywords: ["Next.js", "React", "JavaScript", "Spaced Repetition"],
-  authors: [{ name: "Arvin Garcia", url: "https://arvingarcia.com" }],
-  creator: "Arvin Garcia",
-  publisher: "Arvin Garcia",
+	title: "Reps",
+	description: "A spaced repetition app that supports code syntax highlighting",
+	metadataBase: new URL("https://reps.arvingarcia.com"),
+	applicationName: "Reps",
+	keywords: ["Next.js", "React", "JavaScript", "Spaced Repetition"],
+	authors: [{ name: "Arvin Garcia", url: "https://arvingarcia.com" }],
+	creator: "Arvin Garcia",
+	publisher: "Arvin Garcia",
 };
 
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body className={`${atkinsonHyperlegibleNext.variable} antialiased`}>
-        {children}
-      </body>
-    </html>
-  );
+	return (
+		<html
+			lang="en"
+			className={`${atkinsonHyperlegibleNext.variable} ${atkinsonHyperlegibleMono.variable} ${zalandoSansExpanded.variable} antialiased`}
+		>
+			<body>{children}</body>
+		</html>
+	);
 }
