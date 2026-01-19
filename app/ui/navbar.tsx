0177ec3f6eda38e8fafe84bbc605d7623d741a6c
Arvin Garcia <arvin@arvingarcia.com>
2026-01-19 14:35:00 +0800

    feat: alert dialog before signing out

--- app/ui/navbar.tsx
+++ app/ui/navbar.tsx
@@ -3,6 +3,7 @@
 import Image from "next/image";
 import Link from "next/link";
 import { Menu } from "react-feather";
+import { AlertDialog } from "radix-ui";
 import { useRouter } from "next/navigation";
 
 import type { Session } from "@/lib/definitions";
@@ -15,8 +16,8 @@ export default function NavBar({ session }: { session: Session }) {
 		await authClient.signOut({
 			fetchOptions: {
 				onSuccess: () => {
-					router.refresh();
 					router.push("/");
+					router.refresh();
 				},
 			},
 		});
@@ -50,9 +51,35 @@ export default function NavBar({ session }: { session: Session }) {
 					)}
 					{session && (
 						<li className="ease transition duration-300 hover:opacity-75">
-							<button onClick={handleSignOut} className="cursor-pointer">
-								Sign Out
-							</button>
+							<AlertDialog.Root>
+								<AlertDialog.Trigger asChild>
+									<button className="cursor-pointer">Sign Out</button>
+								</AlertDialog.Trigger>
+								<AlertDialog.Portal>
+									<AlertDialog.Overlay className="fixed inset-0 bg-(--color-black) opacity-75" />
+									<AlertDialog.Content className="fixed top-[50%] left-[50%] flex max-w-80 min-w-80 translate-x-[-50%] translate-y-[-50%] flex-col items-center rounded-2xl border border-(--color-gray-700) bg-(--color-gray-800) p-6 text-(--color-gray-300) outline-none">
+										<AlertDialog.Title className="mb-2 text-2xl font-medium text-(--color-white)">
+											Sign out?
+										</AlertDialog.Title>
+										<AlertDialog.Description className="mb-4 text-center">
+											You are going to sign out of your account. Are you sure?
+										</AlertDialog.Description>
+										<div className="flex gap-4">
+											<AlertDialog.Cancel asChild>
+												<button className="ease cursor-pointer rounded-2xl bg-(--color-gray-700) px-5 py-2 transition duration-300 hover:opacity-75">
+													Cancel
+												</button>
+											</AlertDialog.Cancel>
+											<button
+												onClick={handleSignOut}
+												className="ease cursor-pointer rounded-2xl bg-(--color-primary) px-5 py-2 text-(--color-black) transition duration-300 hover:opacity-75"
+											>
+												Sign out
+											</button>
+										</div>
+									</AlertDialog.Content>
+								</AlertDialog.Portal>
+							</AlertDialog.Root>
 						</li>
 					)}
 				</ul>
