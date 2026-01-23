fc15a3faf31cef60a29ecf2fcdfef01f3100799f
Arvin Garcia <arvin@arvingarcia.com>
2026-01-23 10:47:50 +0800

    feat: delete card

---
+++ app/(main)/deck/[id]/view/page.tsx
@@ -0,0 +1,77 @@
+import { fetchDeck, fetchCards } from "@/lib/queries";
+import { Edit2, Plus } from "react-feather";
+import DeleteCardModal from "@/app/ui/delete-card-modal";
+import Link from "next/link";
+
+export default async function ViewCardsPage({
+	params,
+}: {
+	params: Promise<{ id: string }>;
+}) {
+	const { id } = await params;
+	const deck = await fetchDeck(id);
+
+	if (!deck) return <p>Deck not found.</p>;
+
+	const cards = await fetchCards(id);
+
+	if (!cards) return <p>Cards not found.</p>;
+
+	return (
+		<div className="grid flex-1 place-items-center">
+			{cards.length === 0 ? (
+				<NoCards deck={deck} />
+			) : (
+				<div className="h-full w-full self-start justify-self-start">
+					<div className="mb-2 grid grid-cols-[20px_1fr_1fr_20px_20px] items-center gap-4 border-b border-dashed border-(--color-gray-700) pb-4 text-(--color-gray-300)">
+						<p>#</p>
+						<p>Front</p>
+						<p>Back</p>
+						<span></span>
+						<Link href={`/deck/${id}/create`}>
+							<Plus className="ease h-5 w-5 transition duration-300 hover:stroke-(--color-primary)" />
+						</Link>
+					</div>
+					<div className="flex flex-col gap-2 pt-4">
+						{cards.map((card, index) => (
+							<div
+								key={card.id}
+								className="grid grid-cols-[20px_1fr_1fr_20px_20px] items-center gap-4 rounded-lg text-(--color-gray-300)"
+							>
+								<p>{index + 1}</p>
+								<p>{card.front}</p>
+								<p>{card.back}</p>
+								<Link href={`/deck/${id}/update/${card.id}`}>
+									<Edit2 className="ease h-4 w-4 transition duration-300 hover:stroke-(--color-primary)" />
+								</Link>
+								<DeleteCardModal cardId={card.id} deckId={deck.id} />
+							</div>
+						))}
+					</div>
+				</div>
+			)}
+		</div>
+	);
+}
+
+function NoCards({ deck }: { deck: { id: string; title: string } }) {
+	return (
+		<div className="flex flex-col items-center gap-4">
+			<p className="max-w-3xs text-center text-(--color-gray-300)">
+				No cards yet in your{" "}
+				<Link
+					href={`/deck/${deck.id}`}
+					className="text-(--color-primary) underline hover:no-underline"
+				>
+					{deck.title}
+				</Link>{" "}
+				deck. Add some cards now!
+			</p>
+			<Link href={`/deck/${deck.id}/create`}>
+				<button className="ease cursor-pointer rounded-2xl bg-(--color-primary) px-5 py-2 text-(--color-black) transition duration-300 hover:opacity-75">
+					Add cards
+				</button>
+			</Link>
+		</div>
+	);
+}
---
+++ app/ui/delete-card-modal.tsx
@@ -0,0 +1,67 @@
+"use client";
+
+import { AlertDialog } from "radix-ui";
+import { Trash2 } from "react-feather";
+
+import { useActionState } from "react";
+import { deleteCard } from "@/actions/card-actions";
+
+export default function DeleteCardModal({
+	cardId,
+	deckId,
+}: {
+	cardId: string;
+	deckId: string;
+}) {
+	const [state, formAction] = useActionState(deleteCard, { message: null });
+
+	return (
+		<AlertDialog.Root>
+			<AlertDialog.Trigger asChild>
+				<button className="cursor-pointer hover:text-(--color-primary)">
+					<Trash2 className="h-4 w-4" />
+				</button>
+			</AlertDialog.Trigger>
+			<AlertDialog.Portal>
+				<AlertDialog.Overlay className="fixed inset-0 bg-(--color-black) opacity-75" />
+				<AlertDialog.Content className="fixed top-[50%] left-[50%] flex max-w-80 min-w-80 translate-x-[-50%] translate-y-[-50%] flex-col items-center rounded-2xl border border-(--color-gray-700) bg-(--color-gray-800) p-6 text-(--color-gray-300) outline-none">
+					<AlertDialog.Title className="mb-2 text-2xl font-medium text-(--color-white)">
+						Are you absolutely sure?
+					</AlertDialog.Title>
+					<AlertDialog.Description className="mb-4 text-center">
+						This action cannot be undone. This will permanently delete your
+						card.
+					</AlertDialog.Description>
+					<form action={formAction} className="flex gap-4">
+						<input type="hidden" name="cardId" value={cardId} />
+						<input type="hidden" name="deckId" value={deckId} />
+						{state.errors?.front && (
+							<p className="mt-1 text-sm text-red-500">
+								{state.errors.front[0]}
+							</p>
+						)}
+						{state.errors?.back && (
+							<p className="mt-1 text-sm text-red-500">
+								{state.errors.back[0]}
+							</p>
+						)}
+						{state.message && (
+							<p className="mt-1 text-sm text-red-500">{state.message}</p>
+						)}
+						<AlertDialog.Cancel asChild>
+							<button className="ease cursor-pointer rounded-2xl bg-(--color-gray-700) px-5 py-2 transition duration-300 hover:opacity-75">
+								Cancel
+							</button>
+						</AlertDialog.Cancel>
+						<button
+							type="submit"
+							className="ease cursor-pointer rounded-2xl bg-red-500 px-5 py-2 text-(--color-white) transition duration-300 hover:opacity-75"
+						>
+							Delete card
+						</button>
+					</form>
+				</AlertDialog.Content>
+			</AlertDialog.Portal>
+		</AlertDialog.Root>
+	);
+}
