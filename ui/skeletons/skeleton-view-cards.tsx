c59ad5e0624e7fbd7c95c8bf1b810860e7c731bc
Arvin Garcia <arvin@arvingarcia.com>
2026-02-02 10:35:23 +0800

    feat: implement skeleton loader for pagination

--- ui/skeletons/skeleton-view-cards.tsx
+++ ui/skeletons/skeleton-view-cards.tsx
@@ -1,7 +1,10 @@
+import SkeletonPagination from "./skeleton-pagination";
+
 export default function ViewCardsSkeleton() {
 	return (
 		<div className="grid flex-1 place-items-center">
 			<div className="h-full w-full self-start justify-self-start">
+				<SkeletonPagination />
 				<div className="mb-2 grid grid-cols-[20px_1fr_1fr_20px_20px] items-center gap-4 border-b border-dashed border-(--color-gray-700) pb-4">
 					<div className="h-5 w-5 animate-pulse rounded-full bg-(--color-gray-700)" />
 					<div className="h-5 w-full animate-pulse rounded-md bg-(--color-gray-700)" />
@@ -10,7 +13,7 @@ export default function ViewCardsSkeleton() {
 					<div className="h-5 w-5 animate-pulse rounded-full bg-(--color-gray-700)" />
 				</div>
 				<div className="flex flex-col gap-6 pt-4">
-					{[...Array(20)].map((_, index) => (
+					{[...Array(10)].map((_, index) => (
 						<div
 							key={index}
 							className="grid grid-cols-[20px_1fr_1fr_20px_20px] items-center gap-4 rounded-lg"
