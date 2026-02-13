36bab9c8a1ea6d396cc5a123332e9b59088f4950
Arvin Garcia <arvin@arvingarcia.com>
2026-02-13 17:12:27 +0800

    fix: light/dark ui for not found page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -3,10 +3,10 @@ import Link from "next/link";
 export default function NotFound() {
 	return (
 		<main className="grid h-svh place-items-center">
-			<div className="flex flex-col items-center gap-1">
+			<div className="flex flex-col items-center gap-1 text-(--color-black) dark:text-(--color-white)">
 				<h1 className="text-6xl font-bold">404</h1>
 				<p className="text-(--color-gray-1)">Oops! Page not found.</p>
-				<button className="ease mt-4 cursor-pointer rounded-2xl bg-(--color-primary) px-5 py-2 font-medium text-(--color-bg) transition duration-300 hover:opacity-75">
+				<button className="ease mt-4 cursor-pointer rounded-2xl bg-(--color-primary) px-5 py-2 font-medium text-(--color-bg) transition duration-300 hover:opacity-75 dark:text-(--color-black)">
 					<Link href="/">Go to home</Link>
 				</button>
 			</div>
