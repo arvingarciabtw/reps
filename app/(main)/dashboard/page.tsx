3f7ebe4dded30f8749a8332dbca3ecfe3c0e686f
Arvin Garcia <arvin@arvingarcia.com>
2026-02-14 16:35:58 +0800

    refactor: rewrite description in dashboard page

--- app/(main)/dashboard/page.tsx
+++ app/(main)/dashboard/page.tsx
@@ -2,6 +2,7 @@ import type { DeckType } from "@/lib/definitions";
 import { fetchDecks } from "@/lib/queries";
 import { auth } from "@/lib/auth";
 import { headers } from "next/headers";
+import { Layers } from "react-feather";
 import Deck from "@/ui/deck/deck";
 import CreateDeck from "@/ui/deck/create-deck";
 
@@ -35,8 +36,9 @@ async function Decks() {
 		<>
 			<h1 className="text-4xl font-medium dark:text-(--color-white)">Decks</h1>
 			<p className="mt-4 mb-10 max-w-lg text-(--color-gray-600) dark:text-(--color-gray-300)">
-				All of your decks are below. Click on a deck to interact with its
-				contents. Click on the add button on the bottom right to create a deck.
+				All of your decks are below. Click on a deck to start reviewing. Click
+				on the add button on the bottom right to create a deck. View your cards
+				by clicking on the <Layers className="mx-1 inline h-4.5 w-4.5" /> icon.
 			</p>
 			<section className="grid grid-cols-1 gap-4 xs:grid-cols-2 md:grid-cols-3">
 				{decks.map((deck: DeckType) => (
